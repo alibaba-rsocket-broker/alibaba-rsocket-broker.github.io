a7d5fe81d92b94a67ecea34beac195c5584d9fb2
linux_china <libing.chen@gmail.com>
2021-04-25 17:49:51 -0700

    initial version

---
+++ assets/js/47b7a5c0.59c87031.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{85:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Alibaba RSocket Broker Helm Chart\u53d1\u5e03","permalink":"/blog/2021/04/25/helm-chart"},{"title":"Alibaba RSocket Broker 1.1.0\u53d1\u5e03","permalink":"/blog/2021/04/24/broker-1.1.0-released"}]}')}}]);
---
+++ assets/js/b0f1c099.d2b3b41a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{94:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"kubernetes","name":"kubernetes","count":1,"permalink":"/blog/tags/kubernetes"}')}}]);
